f9ac442d21809af6e55dcbbb98f3f2ce3d96b5ea
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-11-13 22:51:22 +0500

    Task5

---
+++ Tyuiu.VlasenkoAE.Sprint4.Task5.V26.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.VlasenkoAE.Sprint4.Task5.V26.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint4.Task5.V26.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] matrix = new int[5, 5]{ { -4, 2, 1, 0, -3 },
+                                           { 0, 1, 2, 3, 4 },
+                                           { -3, -2, 0, -1, 1 },
+                                           { -4, 3, 2, -1, 0 },
+                                           { -1, 3, 4, 2, -3 }, };
+
+            int[,] res = ds.Calculate(matrix);
+
+            int[,] wait = { { -4, 1, 1, 0, -3 },
+                            { 0, 1, 1, 1, 1 },
+                            { -3, -2, 0, -1, 1 },
+                            { -4, 1, 1, -1, 0 },
+                            { -1, 1, 1, 1, -3 }, };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint4.Task5.V26/Program.cs
@@ -0,0 +1,65 @@
+﻿using Tyuiu.VlasenkoAE.Sprint4.Task5.V26.Lib;
+
+DataService ds = new DataService();
+Random rnd = new Random();
+
+Console.Title = "Спринт #4 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                     *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #26                                                             *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* случайными значениями в диапазоне от -4 до 4.                           *");
+Console.WriteLine("* Заменить положительные элементы на 1.                                  *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Введите количество строк в масиве: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов в масиве: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+int[,] mtrx = new int[rows, columns];
+
+Console.WriteLine("***************************************************************************");
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        mtrx[i, j] = rnd.Next(-4, 4);
+    }
+}
+
+Console.WriteLine("\nМассив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.WriteLine($"{mtrx[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+int[,] result = ds.Calculate(mtrx);
+
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{result[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.ReadKey();
