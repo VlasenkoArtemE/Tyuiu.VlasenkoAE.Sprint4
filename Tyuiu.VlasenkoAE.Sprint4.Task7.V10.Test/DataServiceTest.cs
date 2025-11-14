883de0a2f6dbd3effe8cf9f833e313845e009415
Tyuiu.VlasenkoAE@outlook.com <artemvlasenkoru@mail.ru>
2025-11-14 12:11:11 +0500

    Task7

---
+++ Tyuiu.VlasenkoAE.Sprint4.Task7.V10.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Tyuiu.VlasenkoAE.Sprint4.Task7.V10.Lib;
+
+namespace Tyuiu.VlasenkoAE.Sprint4.Task7.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int rows = 3;
+            int columns = 4;
+            int[,] mtrx = new int[rows, columns];
+            string str = "695847142536";
+
+            int res = ds.Calculate(rows, columns, str);
+            int wait = 30;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.VlasenkoAE.Sprint4.Task7.V10/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.VlasenkoAE.Sprint4.Task7.V10.Lib;
+
+int rows = 3;
+int columns = 4;
+int[,] mtrx = new int[rows, columns];
+string str = "695847142536";
+
+DataService ds  = new DataService();
+
+Console.Title = "Спринт #4 | Выполнил: Власенко А. Е. | ИСТНб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Добавление к решению итоговых проектов по спринту                 *");
+Console.WriteLine("* Задание #7                                                              *");
+Console.WriteLine("* Вариант #10                                                             *");
+Console.WriteLine("* Выполнил: Власенко Артём Евгеньевич | ИСТНб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дана строка из одноразрядных цифр \"695847142536\".                     *");
+Console.WriteLine("* Преобразуйте ее в матрицу 3 на 4 и подсчитайте сумму нечетных чисел.    *");
+Console.WriteLine("*                                                                         *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int index = 0;
+
+Console.WriteLine("\nМассив: ");
+for (int i = 0; i < rows; i++)
+{
+    for (int j = 0; j < columns; j++)
+    {
+        Console.Write($"{str[index]} \t");
+        index++;
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+
+Console.WriteLine("Сумма нечетных чисел: " + ds.Calculate(rows, columns, str));
+
+Console.ReadKey();
